45399d2e41692ac0cfb7732069dc919463fb100a
Barış Özgen <barisozgen@hotmail.com>
2022-11-26 11:54:48 +0300

    SwiftUI and UIKit projects created, assets prepared

---
+++ ChatApp-SwiftUI-UIKit/ChatAppSwiftUI/App/ChatAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppSwiftUIApp.swift
+//  ChatAppSwiftUI
+//
+//  Created by Baris OZGEN on 26.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
